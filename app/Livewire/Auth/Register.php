98697fffafeb99f3746ea2b468cebe319cbcc54e
Asep Saepuloh <mac@macs-MacBook-Pro.local>
2025-08-08 23:08:41 +0700

    default state

---
+++ app/Livewire/Auth/Register.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Livewire\Auth;
+
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Livewire\Attributes\Layout;
+use Livewire\Component;
+
+#[Layout('components.layouts.auth')]
+class Register extends Component
+{
+    public $countries = [];
+    public string $name = '';
+
+    public string $email = '';
+
+    public string $country = '';
+
+    public string $password = '';
+
+    public string $password_confirmation = '';
+
+    public function mount(): void
+    {
+        $this->countries = countries() ?? [];
+    }
+
+    /**
+     * Handle an incoming registration request.
+     */
+    public function register(): void
+    {
+        $validated = $this->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+            'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
+            'country' => ['required', 'string', 'max:255'],
+        ]);
+
+        $validated['password'] = Hash::make($validated['password']);
+
+        event(new Registered(($user = User::create($validated))));
+
+        Auth::login($user);
+
+        $this->redirect(route('dashboard', absolute: false), navigate: true);
+    }
+}
